8b7c81607b22838e53963ec9be687990c5aebf73
mroatder <nattawat.six@gmail.com>
2024-11-17 18:23:06 +0700

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="footer bg-dark text-white py-4">
+      <div class="container text-center">
+        <p>&copy; 2024 Movflix. All rights reserved.</p>
+      </div>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer'
+  }
+  </script>
+  
+  <style scoped>
+  .footer {
+    margin-top: 30px;
+  }
+  </style>
+  
