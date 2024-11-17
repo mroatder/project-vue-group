51158e98176fe04bf54023c3098022fb3e4527d7
mroatder <nattawat.six@gmail.com>
2024-11-17 18:23:08 +0700

    Create Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">Movflix</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Movies</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">TV Shows</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Pricing</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+            <li class="nav-item"><a class="btn btn-primary ms-3" href="#">Sign In</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Header'
+  }
+  </script>
+  
+  <style scoped>
+  .navbar {
+    margin-bottom: 30px;
+  }
+  </style>
+  
